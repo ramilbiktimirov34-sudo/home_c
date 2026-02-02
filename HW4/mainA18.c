b56c58a88bf2ddaa3db9560e04285cc5cb4b4094
Ramil Biktimirov <ramilbiktimirov34@gmail.com>
2026-02-02 21:46:23 +0300

    HomeWork4_A18

---
+++ HW4/mainA18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ int main()
+{
+    int a = 0;
+    int b = 0;
+    scanf("%d%d",&a,&b);
+    if(a > b)
+        printf("Above\n");
+    else if (a < b)
+        printf("Less\n");
+    else if (a == b)
+        printf("Equal\n");
+}
